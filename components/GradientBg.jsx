fc3f2e75697c2b0acf1979c148468236586689b4
Yatik1 <yatiksrivastava1@gmail.com>
2024-11-03 14:53:08 +0530

    Gradient background

---
+++ components/GradientBg.jsx
@@ -0,0 +1,20 @@
+
+export default function GradientBg() {
+    return (
+      <>
+          <div className="absolute top-14 left-[-22rem]">
+          <img 
+            src="https://assets-global.website-files.com/63793925c7db23ce040b0824/64ef089c7513babb5e53bc6c_Meeesh2.jpg"   
+            className="h-[60rem] rotate-[213deg]"
+          />
+        </div>
+        <div className="absolute top-17 right-[-18rem]">
+          <img 
+            src="https://assets-global.website-files.com/63793925c7db23ce040b0824/64ef089c7513babb5e53bc6c_Meeesh2.jpg"   
+            className=" w-[50rem] h-[65rem] rotate-[0deg]"
+          />
+        </div>
+      </>
+    )
+  }
+  
