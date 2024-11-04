cc669119cdb20a7f7b9016a3b2a9dcb570cee69c
Yatik1 <yatiksrivastava1@gmail.com>
2024-11-04 17:05:24 +0530

    Menu util

---
+++ utils/Menu.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+function Menu() {
+  return (
+    <div className='h-fit'>
+         <svg
+    xmlns="http://www.w3.org/2000/svg"
+    x="0px"
+    y="0px"
+    width="20"
+    height="20"
+    viewBox="0 0 256 256"
+  >
+    <g
+      fill="#6f907e"
+      fillRule="nonzero"
+      stroke="none"
+      strokeWidth="1"
+      strokeLinecap="butt"
+      strokeLinejoin="miter"
+      strokeMiterlimit="10"
+      fontFamily="none"
+      fontWeight="none"
+      fontSize="none"
+      textAnchor="none"
+      style={{ mixBlendMode: 'normal' }}
+    >
+      <g transform="scale(9.84615,9.84615)">
+        <path d="M0,4v2h26v-2zM0,12v2h26v-2zM0,20v2h26v-2z"></path>
+      </g>
+    </g>
+  </svg>
+    </div>
+  )
+}
+
+export default Menu
