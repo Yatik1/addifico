f536f1a06a20b0ba2e38e1de3feb47189734d3fe
Yatik1 <yatiksrivastava1@gmail.com>
2024-11-04 17:55:22 +0530

    services section

---
+++ components/Services.jsx
@@ -0,0 +1,17 @@
+
+import Card from './ui/Card'
+
+function Services() {
+  return (
+    <div className='pb-[5.5rem]'>
+        <div className="text-[#DCEED8] font-medium text-[10rem] mt-[5.6rem] ml-[7rem]">Services</div>
+
+        {/* cards */}
+        <div className='flex flex-row items-center ml-[28rem]'>
+            <Card />
+        </div>
+    </div>
+  )
+}
+
+export default Services
