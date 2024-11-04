10ce33daa67e5eabcac59126e68ea93a93b03473
Yatik1 <yatiksrivastava1@gmail.com>
2024-11-04 17:05:05 +0530

    Gradient box

--- components/GradientBg.jsx
+++ components/GradientBg.jsx
@@ -1,7 +1,7 @@
 
 export default function GradientBg() {
     return (
-      <>
+      <div>
           <div className="absolute top-14 left-[-22rem]">
           <img 
             src="https://assets-global.website-files.com/63793925c7db23ce040b0824/64ef089c7513babb5e53bc6c_Meeesh2.jpg"   
@@ -14,7 +14,13 @@ export default function GradientBg() {
             className=" w-[50rem] h-[65rem] rotate-[0deg]"
           />
         </div>
-      </>
+        <div className="absolute top-[68rem] left-[-20rem]">
+          <img 
+            src="https://assets-global.website-files.com/63793925c7db23ce040b0824/64ef089c7513babb5e53bc6c_Meeesh2.jpg"   
+            className="h-[60rem] rotate-[-135deg]"
+          />
+        </div>
+      </div>
     )
   }
   
