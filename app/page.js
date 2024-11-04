07c5668cb383657adb584090a75964d17b5dda6e
Yatik1 <yatiksrivastava1@gmail.com>
2024-11-04 17:04:44 +0530

    page

--- app/page.js
+++ app/page.js
@@ -1,22 +1,12 @@
-
 import GradientBg from "@/components/GradientBg";
-import HeroSection from "@/components/HeroSection";
+import HomePage from "@/components/HomePage";
+
 
 export default function Home() {
   return (
-    <div className="relative w-[100%] h-screen bg-[#272d2a] overflow-x-hidden">
-
-      <GradientBg />
-
-      <div className="absolute h-full w-full">
-        <div className=" w-[97.5%] h-[95%] bg-black opacity-15 rounded-[30px] m-4" />
-      </div>
-
-      <div className="w-full h-full absolute z-10 flex flex-col items-center justify-center">
-        <p className="text-[#637163] text-lg">Competitive Edge through Creativity & Technology</p>
-        <HeroSection />
-      </div>
-
-    </div>
+        <div className="relative w-[100%] h-screen bg-[#272d2a] overflow-x-hidden">
+          <GradientBg />
+          <HomePage />
+        </div>
   );
 }
