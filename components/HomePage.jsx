29edac2b728cf9a1e213148f7a990dac5a4c0539
Yatik1 <yatiksrivastava1@gmail.com>
2024-11-04 17:55:16 +0530

    home page

--- components/HomePage.jsx
+++ components/HomePage.jsx
@@ -1,6 +1,7 @@
 import HeroSection from "@/components/HeroSection";
 import Navbar from "@/components/Navbar";
 import About from "./About";
+import Services from "./Services";
 
 
 function HomePage() {
@@ -10,6 +11,7 @@ function HomePage() {
         <Navbar />
         <div className=" w-[97.5%] h-[80%] bg-black opacity-15 rounded-[30px] m-4" />
         <About />
+        <Services />
       </div>
 
       <div className="w-full h-full absolute z-10 flex flex-col items-center justify-center">
