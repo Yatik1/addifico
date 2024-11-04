fff14ac684a1b4e13a0459bd759d3bb2c2f105e2
Yatik1 <yatiksrivastava1@gmail.com>
2024-11-04 17:05:11 +0530

    Home page

---
+++ components/HomePage.jsx
@@ -0,0 +1,24 @@
+import HeroSection from "@/components/HeroSection";
+import Navbar from "@/components/Navbar";
+import About from "./About";
+
+
+function HomePage() {
+  return (
+    <>
+      <div className="absolute h-screen w-full">
+        <Navbar />
+        <div className=" w-[97.5%] h-[80%] bg-black opacity-15 rounded-[30px] m-4" />
+        <About />
+      </div>
+
+      <div className="w-full h-full absolute z-10 flex flex-col items-center justify-center">
+        <p className="text-[#637163] text-lg">Competitive Edge through Creativity & Technology</p>
+        <HeroSection />
+      </div>
+
+    </>
+  )
+}
+
+export default HomePage
