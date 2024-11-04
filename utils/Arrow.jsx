70a5129579382c0656e075a28b1fd3087bfebd97
Yatik1 <yatiksrivastava1@gmail.com>
2024-11-04 17:55:39 +0530

    arrow utils

---
+++ utils/Arrow.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Arrow() {
+  return (
+    <svg width="0.9em" height="0.9em" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path
+    fillRule="evenodd"
+    clipRule="evenodd"
+    d="M13.5858 2L1.00003 2V0L16 0C16.5523 0 17 0.447715 17 1V16L15 16V3.41421L1.90384 16.5104L0.489624 15.0962L13.5858 2Z"
+    fill="currentColor"
+  ></path>
+</svg>
+
+  )
+}
+
+export default Arrow
