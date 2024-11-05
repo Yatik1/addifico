12ef6e9df40c40fee4b160633224109e3c409add
Yatik1 <yatiksrivastava1@gmail.com>
2024-11-05 20:20:19 +0530

    data obj

---
+++ data/data.js
@@ -0,0 +1,28 @@
+import Bag from '@/utils/Bag'
+import Bar from '@/utils/Bar'
+import Charts from '@/utils/Charts'
+import Goals from '@/utils/Goals'
+import Profile from '@/utils/Profile'
+
+export const cardData = [
+    {
+        logo:<Bar />,
+        title:"Market Trend Analysis"
+    } , 
+    {
+        logo:<Profile />,
+        title:"Customer Reasearch"
+    },
+    {
+        logo:<Goals />,
+        title:"Go-to-Market Strategy"
+    },
+    {
+        logo:<Charts />,
+        title:"Competitive Analysis"
+    },
+    {
+        logo:<Bag />,
+        title:"Business Development Strategy"
+    }
+]
