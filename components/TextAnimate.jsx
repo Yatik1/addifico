8f31768870dad03c65051551e2a72f228573c3f3
Yatik1 <yatiksrivastava1@gmail.com>
2024-11-05 21:01:58 +0530

    Text animation

---
+++ components/TextAnimate.jsx
@@ -0,0 +1,27 @@
+"use client";
+import { motion } from "framer-motion";
+
+function TextAnimate() {
+    let content = "At Addifico Consulting, we use human creativity and the latest technologies to help business leaders, investors, and entrepreneurs enhance their market positioning, discover the next winning trend, and optimize their chances for success."
+    const words = content.split("")
+    
+  return (
+    <p className="text-[3.2em] leading-[4rem] tracking-tighter text-[#39413D]">
+        {words.map((word,index) => (
+            <motion.span 
+                key={index}
+                initial={{opacity:0.5 , color:"#39413D" }}
+                animate={{ opacity:1, color:"#A6B5A3" }}
+                transition={{
+                    duration: 0.4,
+                    delay: index * 0.05,
+                }}
+            >
+                {word}
+            </motion.span>
+        ))}
+    </p>
+  )
+}
+
+export default TextAnimate
