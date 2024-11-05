d202b17ca4d9a5a6939aea7b94026276e509cc9d
Yatik1 <yatiksrivastava1@gmail.com>
2024-11-05 20:19:54 +0530

    services

--- components/Services.jsx
+++ components/Services.jsx
@@ -1,14 +1,19 @@
 
 import Card from './ui/Card'
+import {cardData} from "@/data/data"
 
 function Services() {
   return (
-    <div className='pb-[5.5rem]'>
+    <div className='pb-[5.5rem] w-fit'>
         <div className="text-[#DCEED8] font-medium text-[10rem] mt-[5.6rem] ml-[7rem]">Services</div>
 
-        {/* cards */}
-        <div className='flex flex-row items-center ml-[28rem]'>
-            <Card />
+        <div className='flex flex-row gap-4 items-center ml-[28rem]'>
+            {cardData.map((data) => (
+              <Card 
+                logo={data.logo}
+                title={data.title}
+              />
+            ))}
         </div>
     </div>
   )
