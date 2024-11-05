6a0650a78d15c95389f658ab5706a57f3ae585ad
Yatik1 <yatiksrivastava1@gmail.com>
2024-11-05 20:20:12 +0530

    Card component

--- components/ui/Card.jsx
+++ components/ui/Card.jsx
@@ -1,12 +1,11 @@
 import Arrow from '@/utils/Arrow'
-import Bar from '@/utils/Bar'
 
-const Card = () => {
+const Card = ({logo,title}) => {
   return (
     <div className='w-[20rem] h-[20rem] bg-[#34413A] rounded-[30px] flex flex-col items-start justify-between px-6 pb-8'>
         <div className='w-full flex items-start justify-between pt-5'>
             <div className='text-[#98D18B]'>
-                <Bar />
+                {logo}
             </div>
 
             <div className='text-[#515c51]'>
@@ -15,7 +14,7 @@ const Card = () => {
 
         </div>
         <div className='w-[60%] leading-7'>
-            <p className='text-[1.5rem] w-full text-[#818E81]'>Market Trend Analysis</p>
+            <p className='text-[1.5rem] w-full text-[#818E81]'>{title}</p>
         </div>
     </div>
   )
