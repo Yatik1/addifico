615682b70c4626a90b2943576f431c7126294deb
Yatik1 <yatiksrivastava1@gmail.com>
2024-11-06 14:17:26 +0530

    Home

--- components/HomePage.jsx
+++ components/HomePage.jsx
@@ -1,7 +1,9 @@
-import HeroSection from "@/components/HeroSection";
+
 import Navbar from "@/components/Navbar";
 import About from "./About";
 import Services from "./Services";
+import Record from "./Record";
+import Hero from "@/components/Hero";
 
 
 function HomePage() {
@@ -11,12 +13,13 @@ function HomePage() {
         <Navbar />
         <div className=" w-[97.5%] h-[80%] bg-black opacity-15 rounded-[30px] m-4" />
         <About />
-        <Services />
+        <Services />\
+        <Record />
       </div>
 
       <div className="w-full h-full absolute z-10 flex flex-col items-center justify-center">
         <p className="text-[#637163] text-lg">Competitive Edge through Creativity & Technology</p>
-        <HeroSection />
+        <Hero />
       </div>
 
     </>
