3671b3d17ba67c77cb416c3af25f571bbf89c8fe
Yatik1 <yatiksrivastava1@gmail.com>
2024-11-07 23:21:01 +0530

    card responsive

--- components/ui/Card.jsx
+++ components/ui/Card.jsx
@@ -2,18 +2,18 @@ import Arrow from '@/utils/Arrow'
 
 const Card = ({logo,title}) => {
   return (
-    <div className='w-[20rem] h-[20rem] bg-[#34413A] rounded-[30px] flex flex-col items-start justify-between px-6 pb-8'>
+    <div className='w-full h-[13rem] lg:w-[20rem] lg:h-[20rem] bg-[#34413A] rounded-[30px] flex flex-col items-start justify-between px-6 pb-8'>
         <div className='w-full flex items-start justify-between pt-5'>
             <div className='text-[#98D18B]'>
                 {logo}
             </div>
 
-            <div className='text-[#515c51]'>
+            <div className='text-[#587452]'>
                 <Arrow />
             </div>
 
         </div>
-        <div className='w-[60%] leading-7'>
+        <div className='w-full lg:w-[60%] leading-7'>
             <p className='text-[1.5rem] w-full text-[#818E81]'>{title}</p>
         </div>
     </div>
