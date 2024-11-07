ec335c812e5f4129c7e43c26f286e5257b92f566
Yatik1 <yatiksrivastava1@gmail.com>
2024-11-07 23:20:13 +0530

    Home page

--- components/HomePage.jsx
+++ components/HomePage.jsx
@@ -4,6 +4,8 @@ import About from "./About";
 import Services from "./Services";
 import Record from "./Record";
 import Hero from "@/components/Hero";
+import Resources from "./Resources";
+import Footer from "./ui/Footer";
 
 
 function HomePage() {
@@ -11,15 +13,17 @@ function HomePage() {
     <>
       <div className="absolute h-screen w-full">
         <Navbar />
-        <div className=" w-[97.5%] h-[80%] bg-black opacity-15 rounded-[30px] m-4" />
+        <div className="w-[94%] h-[25rem] md:w-[97.4%] lg:w-[97%] lg:h-[80%] bg-black opacity-15 rounded-[30px] mx-[0.87rem] my-4" />
         <About />
-        <Services />\
+        <Services />
         <Record />
+        <Resources />
+        <Footer />
       </div>
 
-      <div className="w-full h-full absolute z-10 flex flex-col items-center justify-center">
-        <p className="text-[#637163] text-lg">Competitive Edge through Creativity & Technology</p>
-        <Hero />
+      <div className="absolute w-full flex flex-col items-center justify-center gap-[1rem] lg:gap-0 top-[5.25rem] lg:top-1/4 py-[4rem] px-[2.7rem] lg:p-0">
+          <p className="text-[#637163] text-[1.5rem] font-medium lg:text-lg">Competitive Edge through Creativity & Technology</p>
+          <Hero />
       </div>
 
     </>
@@ -27,3 +31,4 @@ function HomePage() {
 }
 
 export default HomePage
+
