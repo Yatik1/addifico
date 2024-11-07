6352efaead0da6401c3c6caa49c59fffa3e045e5
Yatik1 <yatiksrivastava1@gmail.com>
2024-11-07 23:20:01 +0530

    about section

--- components/About.jsx
+++ components/About.jsx
@@ -2,21 +2,21 @@ import TextAnimate from "./ui/TextAnimate";
 
 function About() {
   return (
-    <div className="flex flex-col p-9 mt-[7rem] w-full">
-      <div className="flex items-center justify-start ml-[8rem]">
-        <h2 className="text-[#98D18B] text-[6rem] leading-[7rem] font-medium">
+    <div className="flex flex-col px-4 lg:p-9 mt-[5rem] lg:mt-[7rem] w-full">
+      <div className="flex items-center lg:justify-start lg:ml-[8rem]">
+        <h2 className="text-[#98D18B] text-[4rem] lg:text-[5.5rem] leading-[4rem] lg:leading-[7rem] font-medium">
           Make a <br />
           Difference
         </h2>
       </div>
 
-      <hr className="w-[85%] ml-[8rem] mt-[3rem] border-[#4D534E]" />
+      <hr className="w-[85%] lg:ml-[8rem] mt-[1.5rem] lg:mt-[3rem] border-[#4D534E]" />
 
-      <div className="flex gap-8 ml-[8rem]">
-        <div className="w-[45%] mt-8">
-          <p className="text-[#A6B5A3] text-lg">Why we exist?</p>
+      <div className="flex flex-col lg:flex-row lg:gap-8 lg:ml-[8rem] items-start justify-start px-2 lg:p-0">
+        <div className="lg:w-[45%] mt-8">
+          <p className="text-[#A6B5A3] text-[1.3rem] lg:text-lg">Why we exist?</p>
         </div>
-        <div className="w-[55%] mt-8 mr-[2rem]">
+        <div className="lg:w-[55%] mt-5 lg:mt-8 lg:mr-[2rem]">
           <TextAnimate />
         </div>
         
