dd94a4f5caec315d65c9d7f5e7e1c826f72db46b
Yatik1 <yatiksrivastava1@gmail.com>
2024-11-08 12:07:13 +0530

    Gradient Bg

--- components/ui/GradientBg.jsx
+++ components/ui/GradientBg.jsx
@@ -2,22 +2,40 @@
 export default function GradientBg() {
     return (
       <div>
-          <div className="absolute top-14 left-[-22rem]">
+          <div className="absolute top-10 md:top-20 left-[-22rem]">
           <img 
             src="https://assets-global.website-files.com/63793925c7db23ce040b0824/64ef089c7513babb5e53bc6c_Meeesh2.jpg"   
-            className="h-[60rem] rotate-[213deg]"
+            className="h-[40rem] lg:h-[60rem] rotate-[213deg]"
           />
         </div>
-        <div className="absolute top-17 right-[-18rem]">
+        <div className="absolute top-17 md:top-20 right-[-10rem] md:right-[-18rem]">
           <img 
             src="https://assets-global.website-files.com/63793925c7db23ce040b0824/64ef089c7513babb5e53bc6c_Meeesh2.jpg"   
-            className=" w-[50rem] h-[65rem] rotate-[0deg]"
+            className="w-[50rem] h-[25rem] md:h-[30rem] lg:h-[65rem] rotate-[0eg]"
           />
         </div>
-        <div className="absolute top-[78rem] left-[-20rem]">
+        <div className="hidden md:block absolute top-[56rem] md:top-[78rem] left-[-35rem] lg:left-[-20rem]">
+          <img 
+            src="https://assets-global.website-files.com/63793925c7db23ce040b0824/64ef089c7513babb5e53bc6c_Meeesh2.jpg"   
+            className="h-[60rem] rotate-[70deg] lg:rotate-[-135deg]"
+          />
+        </div>
+        <div className="hidden md:block absolute top-[200rem] right-[-2rem]">
           <img 
             src="https://assets-global.website-files.com/63793925c7db23ce040b0824/64ef089c7513babb5e53bc6c_Meeesh2.jpg"   
             className="h-[60rem] rotate-[-135deg]"
+          />
+        </div>
+        <div className="hidden md:block absolute top-[275rem] left-[-15rem]">
+          <img 
+            src="https://assets-global.website-files.com/63793925c7db23ce040b0824/64ef089c7513babb5e53bc6c_Meeesh2.jpg"   
+            className="h-[27rem] rotate-[360deg]"
+          />
+        </div>
+        <div className="hidden md:block absolute top-[255rem] right-[-20rem]">
+          <img 
+            src="https://assets-global.website-files.com/63793925c7db23ce040b0824/64ef089c7513babb5e53bc6c_Meeesh2.jpg"   
+            className="h-[50rem] rotate-[175deg]"
           />
         </div>
       </div>
